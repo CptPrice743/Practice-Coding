0888ba00d5a724e9c7fce115fdb5ff7d522bd968
Vyom Uchat <vyom.uchat@gmail.com>
2025-02-15 14:53:57 +0530

    Create Kepper Project

---
+++ Web_Development/Udemy/Exercises/React/Project - Keeper/my-react-app/src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note() {
+    return (
+        <div class="note" >
+            <h1>This is a note title</h1>
+            <p>This is note content</p>
+        </div>
+    );
+}
+
+
+export default Note;
